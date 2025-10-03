06e1e0c1b1f68dc831bbf1dbca926255b03b58d5
Matt Gonzales <ttamgonzales@gmail.com>
2025-10-04 05:33:31 +0800

    feat(dbcontext): Add StudentDiaryContext with User and DiaryEntry DbSets

---
+++ StudentDiary.Infrastructure/Data/StudentDiaryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentDiary.Infrastructure.Entities;
+
+namespace StudentDiary.Infrastructure.Data
+{
+    public class StudentDiaryContext : DbContext
+    {
+        public StudentDiaryContext(DbContextOptions<StudentDiaryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+    }
+}
