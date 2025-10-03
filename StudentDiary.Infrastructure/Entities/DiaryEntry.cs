3fd6f7616ef0f63fc06fbf777e4984aa080b0491
Matt Gonzales <ttamgonzales@gmail.com>
2025-10-04 05:11:35 +0800

    feat(entities): Add User and DiaryEntry entity classes

---
+++ StudentDiary.Infrastructure/Entities/DiaryEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary.Infrastructure.Entities
+{
+    internal class DiaryEntry
+    {
+    }
+}
